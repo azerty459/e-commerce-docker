00a9af4987e778effe64a5e12856dd9eca317a29
Olivier DELBOUVE <odelbouve@nextoo.fr>
2019-04-01 18:48:50 +0200

    Correction docker pour l'environnement de DEV

--- docker/java/run.sh
+++ docker/java/run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-echo ">> Waiting for Postgresql to start......................................."
+echo ">>>>> Checking if PostgreSQL is ready before starting."
 WAIT=0
-while ! nc -z postgres_ecommerce_dev 5432; do
+
+until nc -zv postgres_ecommerce_dev 5432; do
+    echo ">>>>> PostgreSQL is not ready yet, waiting 1s."
     sleep 1
     WAIT=$((WAIT + 1))
+    # Wait 15s max
     if [ "$WAIT" -gt 15 ]; then
-        echo "Error: Timeout waiting for Postgresql to start."
+        echo ">>>>> Error: Timeout waiting for PostgreSQL, exiting."
         exit 1
     fi
 done
-cd /var/www/e-commerce
-pwd
-ls -latr
 
-# Lancement du .jar à la racine de l'application en environnement de DEV
-java -jar target/e-commerce-0.0.1-SNAPSHOT.jar --spring.config.location="file:/var/www/e-commerce/src/main/resources/application-dev.properties"
+echo ">>>>> PostgreSQL is ready, launching application"
+
+# Lancement du .jar à la racine de l'application
+
+# En LOCAL
+#java -jar /e-commerce/target/e-commerce-0.0.1-SNAPSHOT.jar --spring.config.location="file:/e-commerce/src/main/resources/application-dev.properties"
+
+# En DEV
+java -jar /e-commerce/e-commerce.jar --spring.config.location="file:/e-commerce/src/main/resources/application-dev.properties"
